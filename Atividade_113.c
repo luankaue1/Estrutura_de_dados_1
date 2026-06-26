c9d21c4a567b00961368af50aaf6608c050d6ef4
luankaue1 <luanksilva85@gmail.com>
2026-06-26 20:02:03 -0300

    Criar Atividade_113.c

---
+++ Atividade_113.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Jogador {
+int id;
+char nickname[50];
+struct Jogador *proximo;
+} Jogador;
+
+typedef struct Fila {
+Jogador *inicio;
+Jogador *fim;
+} Fila;
+
+Fila* criar_fila() {
+Fila *fila = (Fila *)malloc(sizeof(Fila));
+fila->inicio = NULL;
+fila->fim = NULL;
+return fila;
+}
+
+void inserir(Fila *fila, int id, const char *nickname) {
+Jogador *novo = (Jogador *)malloc(sizeof(Jogador));
+novo->id = id;
+strcpy(novo->nickname, nickname);
+novo->proximo = NULL;
+
+if (fila->inicio == NULL) {
+fila->inicio = novo;
+fila->fim = novo;
+} else {
+fila->fim->proximo = novo;
+fila->fim = novo;
+}
+}
+
+void salvarFila(Fila *fila, const char *nomeArquivo) {
+FILE *arquivo = fopen(nomeArquivo, "wb");
+if (arquivo == NULL) {
+printf("Erro ao abrir o arquivo para escrita.\n");
+return;
+}
+
+Jogador *atual = fila->inicio;
+while (atual != NULL) {
+fwrite(atual, sizeof(Jogador), 1, arquivo);
+atual = atual->proximo;
+}
+
+fclose(arquivo);
+printf("Fila salva em %s com sucesso.\n", nomeArquivo);
+}
+
+int main() {
+Fila *fila = criar_fila();
+int opcao;
+
+while (1) {
+printf("Menu:\n");
+printf("1. Adicionar Jogador na Fila\n");
+printf("2. Salvar Fila em Arquivo\n");
+printf("3. Sair\n");
+printf("Escolha uma opção: ");
+scanf("%d", &opcao);
+
+if (opcao == 1) {
+int id;
+char nickname[50];
+printf("Digite o ID e o nickname do jogador: ");
+scanf("%d %s", &id, nickname);
+inserir(fila, id, nickname);
+} else if (opcao == 2) {
+salvarFila(fila, "matchmaking_queue.bin");
+} else if (opcao == 3) {
+break;
+} else {
+printf("Opção inválida. Tente novamente.\n");
+}
+}
+free(fila);
+return 0;
+}
